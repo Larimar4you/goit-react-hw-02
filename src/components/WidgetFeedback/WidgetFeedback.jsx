7e20c66ab843c274141875d0886badc0f145ca42
Larimar4you <Zajanezlaja@gmail.com>
2025-03-25 14:50:44 +0100

    💫 пуш

--- src/components/WidgetFeedback/WidgetFeedback.jsx
+++ src/components/WidgetFeedback/WidgetFeedback.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import styles from './WidgetFeedback.module.css';
 
 const WidgetFeedback = ({ feedback, totalFeedback }) => {
   const positiveFeedback = totalFeedback ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   return (
-    <div>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
-      <p>Total: {totalFeedback}</p>
-      <p>Positive: {positiveFeedback}%</p>
-    </div>
+    <>
+      {totalFeedback > 0 ? (
+        <div className={styles.feedbackContainer}>
+          <p className={styles.feedbackItem}>
+            Good: <span>{feedback.good}</span>
+          </p>
+
+          <p className={styles.feedbackItem}>
+            Neutral: <span>{feedback.neutral}</span>
+          </p>
+
+          <p className={styles.feedbackItem}>
+            Bad: <span>{feedback.bad}</span>
+          </p>
+
+          <p className={styles.feedbackTotal}>Total: {totalFeedback}</p>
+          <p className={styles.feedbackPositive}>Positive: {positiveFeedback}%</p>
+        </div>
+      ) : (
+        <p className={styles.noFeedback}>No feedback given yet.</p>
+      )}
+    </>
   );
 };
 
